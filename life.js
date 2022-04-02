7aafbf63c2e5f572516977be98f1154b2bb94ed0
SX Spy Agent <90962949+SX-9@users.noreply.github.com>
2022-04-02 07:56:04 +0700

    made it!

---
+++ life.js
@@ -0,0 +1,11 @@
+function readMoreJS() {
+  document.getElementById('pJS').innerHTML = "<strong>JavaScript</strong>, often abbreviated <strong>JS</strong>, is a <strong>programming language</strong> that is one of the core technologies of the <strong>World Wide Web</strong>, alongside <strong>HTML</strong> and <strong>CSS</strong>. Over 97% of <strong>websites</strong> use JavaScript on the <strong>client</strong> side for <strong>web page</strong> behavior, often incorporating third-party <strong>libraries</strong>.";
+}
+
+function readMoreCSS() {
+  document.getElementById('pCSS').innerHTML = "<strong>Cascading Style Sheets</strong> (<i>CSS</i>) is a <strong>style sheet language</strong> used for describing the <strong>presentation</strong> of a document written in a <strong>markup language</strong> such as <strong>HTML</strong>. CSS is a cornerstone technology of the <strong>World Wide Web</strong>, alongside HTML and <strong>JavaScript</strong>.";
+}
+
+function readMoreHTML() {
+  document.getElementById('pHTML').innerHTML = "<strong>HyperText Markup Language</strong> (<i>HTML</i>) is the standard <strong>markup language</strong> for documents designed to be displayed in a <strong>web browser</strong>. It can be assisted by technologies such as <strong>Cascading Style Sheets</strong> (<i>CSS</i>) and <strong>scripting languages</strong> such as <strong>JavaScript</strong>.";
+}
